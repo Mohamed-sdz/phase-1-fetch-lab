5f700affd26be8cf77c9a86777f365ffebc664e2
saiderrahmani <didi.mousedidi@outlook.com>
2023-04-23 09:33:24 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => renderBooks(data))
+    .catch(error => console.error(error));
 }
 
 function renderBooks(books) {
@@ -12,6 +14,34 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+fetchBooks();
+
+          
+
+
+  
+  function renderBooks(books) {
+    const main = document.querySelector('main');
+    books.forEach(book => {
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
+    });
+  }
+  
   fetchBooks();
-});
+  
+  
+
+
+ 
+    
+
+ function renderBooks(books) { 
+  const main = document.querySelector('main');
+  books.forEach(book => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
+}
